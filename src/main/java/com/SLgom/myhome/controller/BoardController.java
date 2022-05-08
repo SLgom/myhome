5aea07ea9c41c4ccbfa36992ed15361fbd5260f9
SLgom <kuykuydlsgh12@gmail.com>
2022-05-08 12:03:48 +0900

    v.0.1

--- src/main/java/com/SLgom/myhome/controller/BoardController.java
+++ src/main/java/com/SLgom/myhome/controller/BoardController.java
@@ -1,15 +1,30 @@
 package com.SLgom.myhome.controller;
 
+import com.SLgom.myhome.model.Board;
+import com.SLgom.myhome.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+import java.util.List;
+
+@Controller  //이것은 컨트롤러 라고 선언
 @RequestMapping("/board")
 public class BoardController {
 
-    @GetMapping("/list")
-    public String list() {
-        return "board/list";
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @GetMapping("/list")  //URL요청 연결 "접속할 url 주소를 넣어줘야함" /hi 라고 넣으면 매핑된 html파일을 반환
+    public String list(Model model) {
+        List<Board> boards = boardRepository.findAll();
+        model.addAttribute("boards",boards);
+        return "board/list"; //보여줄 페이지의 이름 / 해당 리턴값을 통해 뷰 페이지를 보여준다
+    }
+    @GetMapping("/form")
+    public String from(Model model){
+        return "board/form";
     }
 }
--- src/main/java/com/SLgom/myhome/controller/HomeController.java
+++ src/main/java/com/SLgom/myhome/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @GetMapping
     public String index(){
-        return "index";
+        return "index"; //뷰 템플릿을 연결 (템플릿의 이름을 리턴하게 하기)
 
     }
 
